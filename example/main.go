c29d545c861574052ba07fa5d8f8181497f7029b
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2022-06-06 22:12:14 +0200

    Use ctx

--- example/main.go
+++ example/main.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"github.com/sgaunet/ratelimit"
 )
 
-// init rateLimit
-var r *ratelimit.RateLimit = ratelimit.New(1*time.Second, 2)
-
 func main() {
-	recurse(1000)
+	// init rateLimit
+	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+	r, _ := ratelimit.New(ctx, 1*time.Second, 2)
+
+	go recurse(r, 1000)
+	time.Sleep(5 * time.Second)
+	r.WaitIfLimitReached()
+	r.WaitIfLimitReached()
+	r.WaitIfLimitReached()
+	r.WaitIfLimitReached()
 }
 
-func recurse(i int) {
+func recurse(r *ratelimit.RateLimit, i int) {
 	r.WaitIfLimitReached() // Just call this function to check if rate limit has been reached or not
 	i--
 	fmt.Println(i, time.Now())
 	if i == 0 {
 		return
 	}
-	recurse(i)
+	recurse(r, i)
 }
--- ratelimit.go
+++ ratelimit.go
@@ -1,55 +1,132 @@
 package ratelimit
 
 import (
+	"context"
+	"errors"
+	"os"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 type RateLimit struct {
 	d     time.Duration
 	limit int
 	ch    chan interface{}
+	ctx   context.Context
+	t     *time.Ticker
+	log   *logrus.Logger
 }
 
-// New return a Ratelimit instance and initialize it
-func New(d time.Duration, limit int) *RateLimit {
-	if limit == 0 || d == 0 {
-		panic("limit/duration cannot be 0")
+// New returns a Ratelimit instance and initialize it
+func New(ctx context.Context, d time.Duration, limit int) (*RateLimit, error) {
+	if limit <= 0 || d <= 0 {
+		return nil, errors.New("ratelimit: duration or limit cannot be <= 0")
 	}
+
 	r := RateLimit{
 		d:     d,
 		limit: limit,
 		ch:    make(chan interface{}, limit),
+		ctx:   ctx,
+		log:   initLog(os.Getenv("RATELIMIT_LOGLEVEL")),
 	}
 	r.backgroundRoutine()
-	return &r
+	r.handleCtx()
+	return &r, nil
 }
 
 // backgroundRoutine launches a goroutine to empty the channel every r.d duration
 func (r *RateLimit) backgroundRoutine() {
+	r.log.Debugln("Start backgroundRoutine")
 	go func() {
-		t := time.NewTicker(r.d)
-		for range t.C {
-			// fmt.Println(tick, len(r.ch))
-			length := len(r.ch)
-			for i := 0; i < length; i++ {
-				<-r.ch
-				// fmt.Println("empty ch", len(r.ch), i)
+		r.t = time.NewTicker(r.d)
+	loop:
+		for {
+			select {
+			case <-r.t.C:
+				r.emptyChan()
+			case <-r.ctx.Done():
+				break loop
 			}
 		}
+		r.log.Debugln("Stop backgroundRoutine")
+	}()
+}
+
+func (r *RateLimit) handleCtx() {
+	go func() {
+		<-r.ctx.Done()
+		r.log.Debugln("Stop Ticker")
+		r.t.Stop()
+		r.log.Debugln("Empty chan")
+		r.emptyChan()
+		r.log.Debugln("End of handleCtx")
 	}()
 }
 
 // WaitIfLimitReached wait if limit has been reached
+// do not use IsLimitReached and WaitIFLimitReached in the same algo
 func (r *RateLimit) WaitIfLimitReached() {
-	r.ch <- struct{}{}
+	select {
+	case <-r.ctx.Done():
+		r.log.Debugln("End WaitIfLimitReached")
+		return
+	default:
+		r.ch <- struct{}{}
+	}
 }
 
-// IsLimitReached returns true if limit hasbeen reached
+// IsLimitReached returns true if limit has been reached
+// do not use IsLimitReached and WaitIFLimitReached in the same algo
 func (r *RateLimit) IsLimitReached() bool {
+	if r.ctx.Err() != nil {
+		// program is going to be terminated
+		return false
+	}
 	select {
 	case r.ch <- struct{}{}:
 		return false
 	default:
 		return true
 	}
+}
+
+func (r *RateLimit) emptyChan() {
+	if r.ctx.Err() == nil {
+		length := len(r.ch)
+		for i := 0; i < length; i++ {
+			_, ok := <-r.ch
+			if !ok {
+				break // channel is closed
+			}
+		}
+	}
+}
+
+func initLog(debugLevel string) *logrus.Logger {
+	l := logrus.New()
+	// Log as JSON instead of the default ASCII formatter.
+	//log.SetFormatter(&log.JSONFormatter{})
+	l.SetFormatter(&logrus.TextFormatter{
+		DisableColors:    false,
+		FullTimestamp:    false,
+		DisableTimestamp: true,
+	})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	l.SetOutput(os.Stdout)
+
+	switch debugLevel {
+	case "info":
+		l.SetLevel(logrus.InfoLevel)
+	case "warn":
+		l.SetLevel(logrus.WarnLevel)
+	case "error":
+		l.SetLevel(logrus.ErrorLevel)
+	default:
+		l.SetLevel(logrus.InfoLevel)
+	}
+	return l
 }
